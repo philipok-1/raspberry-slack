98f30c38fe871ac452881e3242c8064935bb353b
philipok-1 <philipworman@gmail.com>
2017-05-17 17:45:26 +0100

    adding IP address report

--- plugins/plugin_status.py
+++ plugins/plugin_status.py
@@ -12,6 +12,11 @@ def plugin_main(message, host):
 
         cpu_pct = psutil.cpu_percent(interval=1, percpu=False)
         temp = utils.get_temp()
-        host.say("Hi, my CPU is at %s%%.  My temperature is %s.  I've been running for %d seconds since last interrupt.  Total run time %d seconds" %
+        host.say("Hi, my CPU is at %s%%.  My temperature is %s.  I've been running Slack for %d seconds since last interrupt.  Total run time %d seconds" %
         (cpu_pct, temp, host.run_time, host.run_time_total))
+
+    elif re.match(r'.*(IP).*', message, re.IGNORECASE):
+
+        ip=psutil.net_if_addrs()['wlan0'][0][1]
+        host.say("My IP address is "+str(ip))
 
